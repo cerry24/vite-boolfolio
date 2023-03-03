7e027fa6dee38dcd9de8c143b6771ca631dd980d
cerry24 <samuelecerretti@gmail.com>
2023-03-03 16:18:53 +0100

    implement home/projects/about-us pages & relative routes

---
+++ src/pages/AboutUs.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'AboutUs'
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
--- src/router.js
+++ src/router.js
@@ -1,9 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import HomePage from './pages/HomePage.vue';
+import ProjectList from './pages/ProjectList.vue';
+import AboutUs from './pages/AboutUs.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage
+        },
+        {
+            path: '/projects',
+            name: 'projects',
+            component: ProjectList
+        },
+        {
+            path: '/about-us',
+            name: 'aboutUs',
+            component: AboutUs
+        },
+
     ]
 });
 
