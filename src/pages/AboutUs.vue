bfc499a56a834305a39a826c788b86f702bd7096
cerry24 <samuelecerretti@gmail.com>
2023-03-03 16:58:18 +0100

    add content to home page & about-us

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -5,7 +5,12 @@ export default {
 </script>
 
 <template>
+    <h2>About Us</h2>
 
+    <p>
+        This project is made with love by a Boolean student,  
+        <a href="https://github.com/cerry24">check my GitHub</a>
+    </p>
 </template>
 
 <style lang="scss" scoped>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -5,7 +5,9 @@ export default {
 </script>
 
 <template>
+    <h1>Home Page</h1>
 
+    <p>Hello and welcome into the front-end part of project Boolfolio</p>
 </template>
 
 <style lang="scss" scoped>
